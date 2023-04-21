d9d75c63e4b1b0618d709058a98316873d7c769a
Katia Itzel Melo Dominguez <65357557+katiaOlem@users.noreply.github.com>
2023-04-20 18:52:30 -0600

    prueba

--- serviceWorker.js
+++ serviceWorker.js
@@ -28,7 +28,6 @@ const assets = [
     "/katiaolem.github.io/images/left-arrow.png",
     "/katiaolem.github.io/images/right-arrow.png",
     "/katiaolem.github.io/pages/fallback.html",
-    "/katiaolem.github.io/admin/templates/materiales.html"
 
     
 ]
